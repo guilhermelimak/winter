75a01ee7a1ea006b1a24a082fed7c08ea9450b43
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-07 20:47:18 -0300

    Initial commit

---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: 'landing-page',
+}
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
