d6e297834b7e8c223a62f3ab4c9e766d3593da62
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-07 22:57:30 -0300

    style: Fix linter errors

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -1,39 +1,29 @@
 <template>
-  <div id="#app">
-    <router-view></router-view>
-  </div>
+<div id="#app">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
-  import store from 'renderer/vuex/store'
+import store from 'renderer/vuex/store'
+
 export default {
-    store,
-  }
+  store,
+}
 </script>
 
 <style>
-  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+@import url(https://fonts.googleapis.com/css?family=Lato:300);
 
-  * {
-    margin: 0;
-    padding: 0;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
 
-  html,
-  body { height: 100%; }
+html,
+body { height: 100%; }
 
-  body {
-    align-items: center;
-    background:
-      radial-gradient(
-        ellipse at center,
-        rgba(255, 255, 255, 1) 0%,
-        rgba(229, 229, 229, .85) 100%
-      );
-    background-position: center;
-    display: flex;
-    font-family: Lato, Helvetica, sans-serif;
-    justify-content: center;
-    text-align: center;
-  }
+body {
+  font-family: Lato, Helvetica, sans-serif;
+}
 </style>
