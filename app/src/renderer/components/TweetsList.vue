62ff3fa7975f718c7815d6f64c56cbabe0384268
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-07 22:59:19 -0300

    feat: Add initial components

---
+++ app/src/renderer/components/TweetsList.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <tweet v-for="tweet in tweets" />
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    tweets: {
+      required: true,
+      type: Array,
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+</style>
---
+++ app/src/renderer/containers/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <tweets-list></tweets-list>
+</div>
+</template>
+
+<script>
+import TweetsList from '../components/TweetsList'
+
+export default {
+  components: {
+    TweetsList,
+  },
+}
+</script>
+
+<style lang="sass">
+</style>
