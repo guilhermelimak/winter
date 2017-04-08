95a25c1079fc23acdab595d08d406ba32440bd1c
Guilherme Lima <guilhermelimak@gmail.com>
2017-04-07 23:43:49 -0300

    chore: Add bootstrap-sass

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -12,18 +12,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url(https://fonts.googleapis.com/css?family=Lato:300);
-
-* {
-  margin: 0;
-  padding: 0;
-}
+@import './styles/app';
 
 html,
-body { height: 100%; }
-
 body {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   font-family: Lato, Helvetica, sans-serif;
 }
 </style>
